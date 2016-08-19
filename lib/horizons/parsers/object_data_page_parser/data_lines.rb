96eb1d43e12742f14f737c6842df9810c4bf1969
Rémy Hannequin <remy.hannequin@gmail.com>
2016-08-19 15:04:27 +0200

    Change name of attributes returned

--- lib/horizons/parsers/object_data_page_parser/data_lines.rb
+++ lib/horizons/parsers/object_data_page_parser/data_lines.rb
@@ -17,14 +17,89 @@ module Horizons
     def export_values
       values = {}
       lines.each do |line|
-        v1 = line.slice(0..middle - 1).strip
-        v2 = line.slice(middle..-1).strip
-        [v1, v2].each do |v|
+        splitted_line(line).each do |v|
           splitted = v.split(SEPARATOR).map(&:strip)
-          values[splitted.first.to_sym] = splitted.last
+          values[change_name(splitted.first)] = splitted.last
         end
       end
       values
+    end
+
+    def splitted_line(line)
+      v1 = line.slice(0..middle - 1).strip
+      v2 = line.slice(middle..-1).strip
+      [v1, v2]
+    end
+
+    def change_name(old_name)
+      case old_name
+      when 'Mean radius (km)'
+        :mean_radius
+      when 'Density (g cm^-3)'
+        :density
+      when 'Mass (10^23 kg )'
+        :mass
+      when 'Flattening, f'
+        :flattening
+      when 'Volume (x10^10 km^3)'
+        :volume
+      when 'Semi-major axis'
+        :semi_major_axis
+      when 'Sidereal rot. period'
+        :sideral_rotation_period
+      when 'Rot. Rate (x10^5 s)'
+        :rotation_rate
+      when 'Mean solar day'
+        :mean_solar_day
+      when 'Polar gravity ms^-2'
+        :polar_gravity
+      when 'Mom. of Inertia'
+        :moment_of_inertia
+      when 'Equ. gravity  ms^-2'
+        :equator_gravity
+      when 'Core radius (km)'
+        :core_radius
+      when 'Potential Love # k2'
+        :potential_love
+      when 'Grav spectral fact u'
+        :grav_spectral_fact_u
+      when 'Topo. spectral fact t'
+        :topo_spectral_fact_t
+      when 'Fig. offset (Rcf-Rcm)'
+        :figure_offset
+      when 'Offset (lat./long.)'
+        :offset
+      when 'GM (km^3 s^-2)'
+        :standard_gravitational_parameter
+      when 'Equatorial Radius, Re'
+        :equatorial_radius
+      when 'GM 1-sigma (km^3 s^-2)'
+        :gm_1_sigma
+      when 'Mass ratio (Sun/Mars)'
+        :mass_ratio
+      when 'Atmos. pressure (bar)'
+        :atmosphere_pressure
+      when 'Max. angular diam.'
+        :maximum_agular_diameter
+      when 'Mean Temperature (K)'
+        :mean_temperature
+      when 'Visual mag. V(1,0)'
+        :visual_magnitude
+      when 'Geometric albedo'
+        :geometric_albedo
+      when 'Obliquity to orbit'
+        :obliquity_to_orbit
+      when 'Mean sidereal orb per'
+        :mean_sidereal_orbit_period
+      when 'Orbit vel.  km/s'
+        :orbit_velocity
+      when 'Escape vel. km/s'
+        :escape_velocity
+      when "Hill's sphere rad. Rp"
+        :hills_sphere_radius
+      when 'Mag. mom (gauss Rp^3)'
+        :magnitude_moment
+      end
     end
   end
 end
--- spec/parsers/data_lines_spec.rb
+++ spec/parsers/data_lines_spec.rb
@@ -4,13 +4,13 @@ describe Horizons::DataLines do
   context '#export_values' do
     let(:data_lines) do
       data_lines = described_class.new
-      data_lines.lines = ['www = xxx   yyy = zzz']
-      data_lines.middle = 12
+      data_lines.lines = ['Mean radius (km)      = 3389.9(2+-4)    Density (g cm^-3)     =  3.933(5+-4)']
+      data_lines.middle = 36
       data_lines
     end
 
     it 'returns a hash' do
-      expect(data_lines.export_values).to eq({ www: 'xxx', yyy: 'zzz' })
+      expect(data_lines.export_values).to eq({ mean_radius: '3389.9(2+-4)', density: '3.933(5+-4)' })
     end
   end
 
@@ -24,6 +24,20 @@ describe Horizons::DataLines do
     it 'set @middle to the most present value in @middles' do
       data_lines.calculate_middle!
       expect(data_lines.middle).to eq(10)
+    end
+  end
+
+  context '#splitted_line' do
+    let(:data_lines) do
+      data_lines = described_class.new
+      data_lines.middle = 9
+      data_lines
+    end
+
+    let(:line) { 'www = xxx  yyy = zzz' }
+
+    it 'returns an array of the splitted line' do
+      expect(data_lines.splitted_line(line)).to match_array(['www = xxx', 'yyy = zzz'])
     end
   end
 end
